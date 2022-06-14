56d07bf6236595ad59e4d71bdafeadb2bb7bc2e2
sarthak-kalra <51453502+sarthak-kalra@users.noreply.github.com>
2022-06-14 20:56:44 +0530

    Time: 2 ms (41.32%), Space: 55.2 MB (5.51%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,47 +1,34 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-        boolean rowt = false;
-        boolean colt = false;
-        for(int row =0; row< arr.length;row++){
-           for(int col =0;col<arr[0].length;col++){
-               if(row==0){
-                   if(arr[row][col]==0){
-                       rowt = true;
-                   }
-               }
-               if(col==0){
-                   if(arr[row][col]==0){
-                       colt=true;
-                   }
-               }
-               else{
-                    if(arr[row][col]==0){
-                        arr[row][0]=0;
-                        arr[0][col]=0;
+        boolean col = false;
+        for(int i =0;i<arr.length;i++){
+                for(int j = 0;j<arr[0].length;j++){
+                    if(j==0){
+                      if(arr[i][j]==0){
+                        col = true;
+                      }    
+                    }else{
+                    if(arr[i][j]==0){
+                        arr[i][0]=0;
+                        arr[0][j]=0;
                     }
-               }
-           }
+                    }
+                
+            }
         }
         
-        for(int row = arr.length-1;row>=0;row--){
-            for(int col = arr[0].length-1;col>=0;col--){
-                if(row==0){
-                    if(rowt==true){
-                        arr[row][col]=0;
+        for(int i = arr.length-1;i>=0;i--){
+            for(int j = arr[0].length-1;j>=0;j--){
+                if(j==0){
+                    if(col==true){
+                        arr[i][j]=0;
                     }
-                }
-                if(col==0){
-                    if(colt==true){
-                        arr[row][col]=0;
-                    }
-                }
-                else{
-                    if(arr[row][0]==0||arr[0][col]==0){
-                        arr[row][col]=0;
+                }else{
+                    if(arr[i][0]==0||arr[0][j]==0){
+                        arr[i][j]=0;
                     }
                 }
             }
-        }    
-       
+        }
     }
 }
