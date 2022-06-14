f3d326d5dc91cfdfd4da8aefe6bdb50f94b32a00
sarthak-kalra <51453502+sarthak-kalra@users.noreply.github.com>
2022-06-14 20:51:12 +0530

    Time: 2 ms (41.32%), Space: 54.9 MB (5.51%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -2,52 +2,46 @@ class Solution {
     public void setZeroes(int[][] arr) {
         boolean rowt = false;
         boolean colt = false;
-        for(int i =0;i<arr.length;i++){
-            if(arr[i][0]==0){
-                rowt = true;
-                break;
-            }
-        }
-        for(int j = 0;j<arr[0].length;j++){
-            if(arr[0][j]==0){
-                colt = true;
-                break;
-            }
-        }
         for(int row =0; row< arr.length;row++){
            for(int col =0;col<arr[0].length;col++){
-               if(arr[row][col]==0){
-                   arr[row][0]=0;
-                   arr[0][col]=0;
+               if(row==0){
+                   if(arr[row][col]==0){
+                       rowt = true;
+                   }
+               }
+               if(col==0){
+                   if(arr[row][col]==0){
+                       colt=true;
+                   }
+               }
+               else{
+                    if(arr[row][col]==0){
+                        arr[row][0]=0;
+                        arr[0][col]=0;
+                    }
                }
            }
         }
         
-        
-        for(int i =1;i<arr.length;i++){
-            if(arr[i][0]==0){
-                for(int j =0;j<arr[0].length;j++){
-                      arr[i][j]=0;
+        for(int row = arr.length-1;row>=0;row--){
+            for(int col = arr[0].length-1;col>=0;col--){
+                if(row==0){
+                    if(rowt==true){
+                        arr[row][col]=0;
+                    }
                 }
-            }   
-        }
-        
-        for(int i =1;i<arr[0].length;i++){
-            if(arr[0][i]==0){
-                for(int j =0;j<arr.length;j++){
-                    arr[j][i]=0;
+                if(col==0){
+                    if(colt==true){
+                        arr[row][col]=0;
+                    }
+                }
+                else{
+                    if(arr[row][0]==0||arr[0][col]==0){
+                        arr[row][col]=0;
+                    }
                 }
             }
-        }
-        if(rowt){
-            for(int i =0;i<arr.length;i++){        
-            arr[i][0]=0;
-            }
-        }
-        if(colt){
-            for(int j = 0;j<arr[0].length;j++){
-                arr[0][j]=0;
-            }
-        }
+        }    
+       
     }
 }
