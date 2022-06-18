560e258d94119d66a6dddfc531b5210e60ade077
sarthak-kalra <51453502+sarthak-kalra@users.noreply.github.com>
2022-06-18 23:28:55 +0530

    Time: 14 ms (47.85%), Space: 55.4 MB (31.90%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,37 +1,25 @@
 class Solution {
-    public int[][] merge(int[][] arr) {
+    public int[][] merge(int[][] intervals) {
         
-       Arrays.sort(arr, Comparator.comparingInt(o -> o[0]));
-      //  Arrays.sort(arr, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-     
-        
-        int count = 0;
-        for(int i =1;i<arr.length;i++){
-        if(count<arr.length&&arr[count][1]>=arr[i][0]){
-            arr[count][1]=Math.max(arr[i][1], arr[count][1]);
-        }
-         else{
-            count++;
-            arr[count][0]=arr[i][0];
-            arr[count][1]=arr[i][1];
-            
-        }
-            
-            
-        }
-        
-    //    System.out.println(count);
-        int[][] res = new int[count+1][2];
-        
-        for (int i = 0; i <= count; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
-			//	System.out.print(arr[i][j] + " ");
-                  res[i][j]= arr[i][j];
+         if (intervals.length <= 1)
+			return intervals;
+
+		// Sort by ascending starting point
+		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+		List<int[]> result = new ArrayList<>();
+		int[] newInterval = intervals[0];
+		result.add(newInterval);
+		for (int[] interval : intervals) {
+			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
+				newInterval[1] = Math.max(newInterval[1], interval[1]);
+			else {                             // Disjoint intervals, add the new interval to the list
+				newInterval = interval;
+				result.add(newInterval);
 			}
-		//	System.out.println();
 		}
-        
-        return res;
+
+		return result.toArray(new int[result.size()][]);
         
     }
 }
